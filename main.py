002dd92bada7fda9ff23a8b4e8216b0355f10f82
mitang0789 <hindochamitang@gmail.com>
2024-08-06 00:06:07 +0530

    V2
    V2

--- main.py
+++ main.py
@@ -2,19 +2,28 @@
 import os
 from datetime import datetime
 
+
 class User:
-    def __init__(self, username):
+    def __init__(self, username, password):
         self.username = username
+        self.password = password
+
 
 class ChatRoom:
     def __init__(self):
-        self.users = []
+        self.users = {}
         self.messages = []
         self.chat_history_file = "chat_history.txt"
         self.load_chat_history()
 
-    def add_user(self, user):
-        self.users.append(user)
+    def add_user(self, username, password):
+        self.users[username] = User(username, password)
+
+    def authenticate_user(self, username, password):
+        user = self.users.get(username)
+        if user and user.password == password:
+            return True
+        return False
 
     def add_message(self, username, message):
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -38,16 +47,27 @@ class ChatRoom:
                     username, message = rest.split(": ", 1)
                     self.messages.append((username, message, timestamp))
 
+
 def main():
     chat_room = ChatRoom()
     print("Welcome to the Chat Room!")
     while True:
+        choice = input("Do you have an account? (yes/no): ").strip().lower()
+        if choice == 'no':
+            username = input("Choose a username: ")
+            password = input("Choose a password: ")
+            chat_room.add_user(username, password)
+            print("Account created successfully!")
         username = input("Enter your username: ")
-        user = User(username)
-        chat_room.add_user(user)
-        message = input("Enter your message: ")
-        chat_room.add_message(username, message)
-        chat_room.display_messages()
+        password = input("Enter your password: ")
+        if chat_room.authenticate_user(username, password):
+            print("Login successful!")
+            message = input("Enter your message: ")
+            chat_room.add_message(username, message)
+            chat_room.display_messages()
+        else:
+            print("Invalid username or password. Try again.")
+
 
 if __name__ == "__main__":
     main()
